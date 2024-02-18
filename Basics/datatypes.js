664898eab2596520beae7d2719770fe5306001ec
harsh Parmar <parmarharsh213@gmail.com>
2024-02-18 14:51:23 +0530

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"  // treats all JS code as newer version
+
+// alert("this is alert") // using nodejs not browser
+
+
+let name = "harsh"
+let age = 20
+let isLoggedin = false
+
+
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true/false 
+// null  => standalone value  // null is an obj in output
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);
