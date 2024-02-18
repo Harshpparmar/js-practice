d2806f39be074f283dbedc2fb0af83a2910f9b3f
harsh Parmar <parmarharsh213@gmail.com>
2024-02-18 15:12:26 +0530

    conversions

---
+++ Basics/conversion_op.js
@@ -0,0 +1,33 @@
+/*
+let score = "33abc" 
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+//  " 33" => 33 
+//  "33abs" => NaN 
+// true => 1; false => 0
+
+/*
+let isLoggedin = "harsh"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+*/
+
+//  1 => true; 0 => false 
+// "" => false
+// "harsh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log( typeof stringNumber);
