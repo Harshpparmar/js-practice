65ba0eea16ab0101afd4ae99f7e80f7ba6aaa5d4
harsh Parmar <parmarharsh213@gmail.com>
2024-02-19 22:38:57 +0530

    prefix postfix

--- Basics/conversion_op.js
+++ Basics/conversion_op.js
@@ -25,9 +25,66 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "harsh" => true
 
+/*
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 
 console.log( typeof stringNumber);
+*/
+
+
+// //// OPERATIONS 
+
+let value = 3 
+
+let negValue = -value 
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello "
+// let str2 = "harsh"
+
+// let str3 = str1 + str2 
+
+// console.log(str3);
+// console.log(1 + "2");
+
+
+let gameCounter = 100 
+
+// gameCounter++;
+// ++gameCounter;
+
+// console.log(gameCounter);
+
+
+// js mdn assignment :-
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x} , y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+/* 
+// UNDERSTANDIND PREFIX POSTFIX
+
+in the prefix the operator is used before the value or operand (++x) "the increment operator increments the value after incrementing."
+
+in the postfix the operator is after the operand of value (x++) "the incremnt operator 
+increments and returns the value before incrementing."
+
+*/
