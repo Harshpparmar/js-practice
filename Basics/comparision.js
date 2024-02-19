756fb321f30c3ad6af664975b731bcbf864376c6
harsh Parmar <parmarharsh213@gmail.com>
2024-02-19 22:56:23 +0530

    understanding comparisions and equality checks

---
+++ Basics/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > "3" );
+// console.log("02" > "1" );
+
+console.log(null > 0); //avoid this types(with undefined too) of conversions this can put you in confusion 
+console.log(null == 0);
+console.log(null >= 0);
+
+
+/* 
+
+in js the comparision and equality check are two different things or concepts 
+
+*/
+
+// strict check ===
+
+
+console.log("2" === 2 );
+console.log(2 === 2 );
