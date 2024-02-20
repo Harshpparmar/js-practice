68d064398dae35737f054435d94e63f192ce5f8b
harsh Parmar <parmarharsh213@gmail.com>
2024-02-20 14:00:13 +0530

    summary of datatypes

--- Basics/comparision.js
+++ Basics/comparision.js
@@ -23,3 +23,60 @@ in js the comparision and equality check are two different things or concepts
 
 console.log("2" === 2 );
 console.log(2 === 2 );
+
+
+/* 
+//  DATA TYPES summary \\
+
+
+
+catagoriesed into two types:
+
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+1) primitive 
+    > 7 types of primitive are there.
+    > String , Number , Boolean , null , undefined , Symbol , BigInt 
+
+
+
+2) non-premitive OR Reference :
+    > Array , Objects , Functions 
+
+
+*/
+const heros = ["ironman","batman", "superman", "thor"];
+
+let obj = {
+    name: "harsh",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello");
+}
+
+
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+// console.log(id === anotherId);
+
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+    Number => number
+    String  => string
+    Boolean  => boolean
+    null  => object
+    undefined  =>  undefined
+    Symbol  =>  symbol
+    BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+    Arrays  =>  object
+    Function  =>  function
+    Object  =>  object
+
+ */
