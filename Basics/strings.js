94a208756a5552bd6057660b225fe6c38db01dca
harsh Parmar <parmarharsh213@gmail.com>
2024-02-21 21:23:37 +0530

    more string methods

--- Basics/strings.js
+++ Basics/strings.js
@@ -5,7 +5,7 @@ const repoCount = 10
 
 // console.log(`hello my name is ${name} and my repo cound is ${repoCount}`);
 
-const gameName = new String('harsh-parmar') 
+const gameName = new String('harsh-parmar-php') 
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -31,3 +31,5 @@ const url = "https://harsh.com/harsh%20parmar"
 console.log(url.replace('%20' , '-'));
 
 console.log(url.includes('harsh'));
+
+console.log(gameName.split('-'));
