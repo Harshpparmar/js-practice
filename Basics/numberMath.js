b565e320737fa057d6d3963e6b130487cf84fdf9
harsh Parmar <parmarharsh213@gmail.com>
2024-02-22 14:23:43 +0530

    maths

---
+++ Basics/numberMath.js
@@ -0,0 +1,39 @@
+const score = 500
+// console.log(score);
+
+
+const balane = new Number(100)
+console.log(balane);
+
+// console.log(balane.toString().length);
+// console.log(balane.toFixed(2));
+
+const otherNumber = 128.9987
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//---------------MATHS---------------\\ 
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.55));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4 , 5 ,3 ,4 , 5,2));
+// console.log(Math.max(4 , 5 ,3 ,4 , 5,2));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
