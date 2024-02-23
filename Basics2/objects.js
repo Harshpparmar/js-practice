6f3e4e312d6e1485e23acd0f85d1b1c9855ebf39
harsh Parmar <parmarharsh213@gmail.com>
2024-02-23 12:38:21 +0530

    more about singleton

--- Basics2/objects.js
+++ Basics2/objects.js
@@ -34,12 +34,81 @@ Jsuser.email = "hp@chatgmail.com"
 
 
 Jsuser.greeting = function() {
-    console.log("hello harsh ");
+    // console.log("hello harsh ");
 }
 
 Jsuser.greetingTo = function() {
-    console.log(`hello user,${this.name}`);
+    // console.log(`hello user,${this.name}`);
 }
 
-console.log(Jsuser.greeting());
-console.log(Jsuser.greetingTo());
+// console.log(Jsuser.greeting());
+// console.log(Jsuser.greetingTo());
+
+
+
+/////// \\\\\\\
+// more about singleton 
+
+// const metaUser = new Object()
+
+const metaUser = {}
+
+metaUser.id = "123hpp"
+metaUser.name = "metaharsh"
+metaUser.isLoggedIn = "false"
+
+
+// console.log(metaUser);
+
+const regularUser = {
+    email : "metahp@meta.com",
+    fullname : {
+        userfullname: {
+            firstname: "harsh",
+            lastname: "parmar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1:"a" , 2:"b"}
+const obj2 = {3:"a" , 4:"b"}
+const obj4 = {5:"a" , 6:"b"}
+
+// const obj3 = Object.assign({}, obj1 , obj2 , obj4)
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email:"hp@gmail.com"
+    },
+    {
+        id: 2,
+        email:"hp2@gmail.com"
+    },    {
+        id: 3,
+        email:"hp3@gmail.com"
+    },    {
+        id: 4,
+        email:"hp4@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(metaUser);
+
+
+// console.log(Object.keys(metaUser)); // imp for database 
+// console.log(Object.values(metaUser));
+// console.log(Object.entries(metaUser));
+
+
+// console.log(metaUser.hasOwn('isLogged'));
+
+
+console.log(metaUser.hasOwnProperty('isLoggedIn'));
+
